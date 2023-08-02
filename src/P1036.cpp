029a80756564b46b4434dfb51e541c837b15c0db
jackfiled <xcrenchangjun@outlook.com>
2023-08-02 11:49:20 +0800

    20230802 Finished

---
+++ src/P1036.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cmath>
+
+bool isPrime(int number)
+{
+    for (int i = 2; i < sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+class DFS {
+public:
+    int number;
+    int target;
+    int result = 0;
+    int *array;
+
+    void dfs(int now, int sum, int next)
+    {
+        if (now == target)
+        {
+            // 达到目标数量
+            if (isPrime(sum))
+            {
+                result++;
+            }
+
+            return;
+        }
+
+        for (int i = next; i < number; i++)
+        {
+            dfs(now + 1, sum + array[i], i + 1);
+        }
+    }
+};
+
+int main()
+{
+    DFS dfs;
+    scanf("%d %d", &dfs.number, &dfs.target);
+    int array[dfs.number];
+    dfs.array = array;
+
+    for (int i = 0; i < dfs.number; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    dfs.dfs(0, 0, 0);
+    printf("%d\n", dfs.result);
+    return 0;
+}
---
+++ src/P1157.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class DFS {
+public:
+    int number;
+    int target;
+
+    void dfs(int now, int next)
+    {
+        if (now == target)
+        {
+            for (auto i : queue)
+            {
+                cout << setw(3) << i;
+            }
+            cout << endl;
+            return;
+        }
+
+        for (int i = next; i <= number; i++)
+        {
+            
+            queue.push_back(i);
+            dfs(now + 1, i + 1);
+            queue.pop_back();
+        }
+    }
+
+private:
+    vector<int> queue;
+};
+
+int main()
+{
+    DFS dfs;
+    scanf("%d %d", &dfs.number, &dfs.target);
+
+    dfs.dfs(0, 1);
+    return 0;
+}
